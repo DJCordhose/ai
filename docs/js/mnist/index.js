7c78bc69e2c1a6cd428de447550099234907cb19
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2018-04-03 17:18:43 +0200

    wip

--- docs/js/mnist/index.js
+++ docs/js/mnist/index.js
@@ -27,40 +27,6 @@ const ui = {
   showTestResults,
 };
 
-const model = tf.sequential();
-
-model.add(tf.layers.conv2d({
-  inputShape: [28, 28, 1],
-  kernelSize: 5,
-  filters: 8,
-  strides: 1,
-  activation: 'relu',
-  kernelInitializer: 'varianceScaling'
-}));
-model.add(tf.layers.maxPooling2d({poolSize: [2, 2], strides: [2, 2]}));
-model.add(tf.layers.conv2d({
-  kernelSize: 5,
-  filters: 16,
-  strides: 1,
-  activation: 'relu',
-  kernelInitializer: 'varianceScaling'
-}));
-model.add(tf.layers.maxPooling2d({poolSize: [2, 2], strides: [2, 2]}));
-model.add(tf.layers.flatten());
-model.add(tf.layers.dense(
-    {units: 10, kernelInitializer: 'varianceScaling', activation: 'softmax'}));
-
-const LEARNING_RATE = 0.15;
-const optimizer = tf.train.sgd(LEARNING_RATE);
-model.compile({
-  optimizer: optimizer,
-  loss: 'categoricalCrossentropy',
-  metrics: ['accuracy'],
-});
-
-const BATCH_SIZE = 64;
-const TRAIN_BATCHES = 150;
-
 // Every few batches, test accuracy over many examples. Ideally, we'd compute
 // accuracy over the whole test set, but for performance we'll use a subset.
 const TEST_BATCH_SIZE = 1000;
